2f12f43c5039b2dc8587d7f931cfb22c17f7cb49
Waleed Fadl <WwaleedFadl@gmail.com>
2024-03-13 09:40:39 +0200

    adding the search part one of it

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import * as modal from './model.js';
+import * as model from './model.js';
 import recipeView from './Views/recipeView.js';
-//
-//
-//
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
+import searchView from './Views/searchView.js';
 // Api
 const controlRecipes = async function () {
   try {
@@ -18,16 +12,29 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     //1) loading the recipe
-    await modal.loadRecipe(id);
-
+    await model.loadRecipe(id);
     // 2) Rendering the recipe
-    recipeView.render(modal.state.recipe);
+    recipeView.render(model.state.recipe);
   } catch (error) {
     recipeView.renderError();
   }
 };
-
+const controlSearchResults = async function () {
+  try {
+    // 1) get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    // 2) Load search results
+    await model.loadSearchResults(query);
+    // 3) Render the results
+    console.log(model.state.search.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+
+  searchView.addHandelerSearch(controlSearchResults);
 };
 init();
