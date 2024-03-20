3787710ebf9ada967311994e4f6159afe069a89d
Waleed Fadl <WwaleedFadl@gmail.com>
2024-03-20 18:37:27 +0200

    edit A

---
+++ src/js/Views/bookMarksView.js
@@ -0,0 +1,17 @@
+import View from './View';
+import previewView from './previewView.js';
+import icons from '../../img/icons.svg';
+class BookMarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No Bookmarks yet, find anice recipe and bookmark it.';
+  _message = '';
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+  _generateMarkup() {
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+export default new BookMarksView();
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '23ce734a-ffde-4413-94cb-dfc1f9485a42';
+export const MODAL_CLOSE_SEC = 2.5;
